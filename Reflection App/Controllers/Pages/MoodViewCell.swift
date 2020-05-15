131cd83f9c3400652f22eeb1416679f136e6aaf0
Lorenzo Masucci <lormas91@gmail.com>
2020-05-15 15:51:51 +0200

    Collection View ready

---
+++ Reflection App/Controllers/Pages/MoodViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MoodViewCell.swift
+//  Reflection App
+//
+//  Created by Lorenzo on 15/05/2020.
+//  Copyright © 2020 Giovanni Prisco. All rights reserved.
+//
+
+import UIKit
+
+class MoodViewCell: UICollectionViewCell {
+    
+}
