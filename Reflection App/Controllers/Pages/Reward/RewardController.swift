bf64b11ec5e7919e572c4429116cb8e76f1f5e37
Lorenzo Masucci <lormas91@gmail.com>
2020-05-19 16:56:06 +0200

    Plume Animation (Not completed with all sizes) + Label and Text View with Constraints for all devices

--- Reflection App/Controllers/Pages/Reward/RewardController.swift
+++ Reflection App/Controllers/Pages/Reward/RewardController.swift
@@ -11,18 +11,34 @@ import UIKit
 class RewardController: UIViewController {
     @IBOutlet var backgroundImage: UIImageView!
     @IBOutlet weak var perlaImage: UIImageView!
+    @IBOutlet weak var textViewReward: UITextView!
+    @IBOutlet weak var labelReward: UILabel!
+    @IBOutlet weak var plumeImage: UIImageView!
+    
     
     var imageName: String!
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         self.backgroundImage.image = UIImage(named: self.imageName)
+        
+        plumeAnimation()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    func plumeAnimation()  {
+
+        UIView.animate(withDuration: 4.0, delay: 0.2, options: [.curveEaseInOut], animations: {
+            self.plumeImage.frame = CGRect(x: self.view.frame.width / 2 , y: self.perlaImage.frame.origin.y , width: 100, height: 200)
+            self.plumeImage.alpha = 0
+        })
         
     }
+
 }
