31b4f0fb33eb0a07757d3524e6ba6bf907484936
Lorenzo Masucci <lormas91@gmail.com>
2020-06-04 15:37:09 +0200

    Details View

---
+++ Reflection App/Controllers/Pages/Reflection Details/ReflectionDetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ReflectionDetailsViewController.swift
+//  Reflection App
+//
+//  Created by Lorenzo on 04/06/2020.
+//  Copyright © 2020 Giovanni Prisco. All rights reserved.
+//
+
+import UIKit
+
+class ReflectionDetailsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+    super.viewDidLoad()
+
+    }
+}
